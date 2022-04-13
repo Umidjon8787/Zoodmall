9843a0580106cd01f6ad6390208a38f906cf72d5
Umidjon8787 <100425651+Umidjon8787@users.noreply.github.com>
2022-04-13 17:51:58 +0300

    asqfdada

--- js/script.js
+++ js/script.js
@@ -12,21 +12,30 @@
     
 // }
 
+window.addEventListener('DOMContentLoaded', function () {
+  window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0)
+  })
+})
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  function filterFunction() {
+  function filterFunction() 
+  {
     var input, filter, ul, li, a, i;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     div = document.getElementById("myDropdown");
     a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
+    for (i = 0; i < a.length; i++) 
+    {
       txtValue = a[i].textContent || a[i].innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
         a[i].style.display = "";
-      } else {
+    } else {
         a[i].style.display = "none";
       }
     }
